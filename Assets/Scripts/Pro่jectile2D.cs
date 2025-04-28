b2699d5ddfbcccdff894aaf5bfee90ee5d571acb
Puttirat <puttirat.lima@bumail.net>
2025-04-28 20:16:09 +0700

    test and resolve bullet

--- Assets/Scripts/Pro่jectile2D.cs
+++ Assets/Scripts/Pro่jectile2D.cs
@@ -8,6 +8,43 @@ public class Pro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+            
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                //show target at click point
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit " + hit.collider.name);
+                
+                //calculate projectile velocity
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shooterPoint.position, hit.point, 1f);
+                
+                //shoot bullet prefab using rigibody2D
+                Rigidbody2D shooterBullet = Instantiate(bulletPrefab, shooterPoint.position, Quaternion.identity);
+                
+                //add projectile velocity vector to the bullet rigibody
+                shooterBullet.linearVelocity = projectileVelocity;
+            }
+        }
     }
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+        
+        //find velocity of x and y axis
+        float velocityX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+        
+        //get projectile vector
+        Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+        
+        return projectileVelocity;
+    }
+    
 }
